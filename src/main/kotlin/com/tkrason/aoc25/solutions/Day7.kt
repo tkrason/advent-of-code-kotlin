750e2b6c89410ae62ac4f1b50aba41ba8ac49cf1
Tomas Krason <tomas.krason@cleevio.com>
2025-12-07 15:44:13 +0100

    Day 7 - part 2

--- src/main/kotlin/com/tkrason/aoc25/solutions/Day7.kt
+++ src/main/kotlin/com/tkrason/aoc25/solutions/Day7.kt
@@ -36,10 +36,10 @@ class Day7 : Solution {
 				}
 
 				if (action == SPLITTER) {
-					if(stateAbove == BEAM) {
-						updatedState[index-1] = BEAM
+					if (stateAbove == BEAM) {
+						updatedState[index - 1] = BEAM
 						updatedState[index] = EMPTY_SPACE
-						updatedState[index+1] = BEAM
+						updatedState[index + 1] = BEAM
 
 						return@foldIndexed acc + 1
 					}
@@ -55,6 +55,96 @@ class Day7 : Solution {
 	}
 
 	override fun solvePart2(input: String) {
-		TODO("Not yet implemented")
+		val lines = input.lines()
+		val emptyState = lines[1].toCharArray() // full of .
+		val linesWithSplitters = lines.filterIndexed { index, _ -> index % 2 == 0 }
+
+		// TODO: A lot of space for optimization by not copying arrays
+		val sum = countTimelines(
+			linesWithSplitters = linesWithSplitters,
+			previousState = emptyState,
+			cache = mutableMapOf()
+		)
+
+		require(sum == 16937871060075L)
+	}
+
+	private fun countTimelines(
+		linesWithSplitters: List<String>,
+		previousState: CharArray,
+		cache: MutableMap<Pair<Int, Int>, Long>,
+	): Long {
+
+		if (linesWithSplitters.isEmpty()) return 1L
+
+		val currentLine = linesWithSplitters.first()
+		val depth = linesWithSplitters.size
+
+		val sum = currentLine.mapIndexed { actionIndex, action ->
+			if (action == EMPTY_SPACE) {
+				val stateAbove = previousState[actionIndex]
+
+				return@mapIndexed if (stateAbove == BEAM) {
+					val cacheKey = depth to actionIndex
+					cache.getOrPut(cacheKey) {
+						countTimelines(
+							linesWithSplitters = linesWithSplitters.drop(1),
+							previousState = previousState.copyOf(),
+							cache = cache
+						)
+					}
+				} else 0L
+			}
+
+			if (action == SOURCE) {
+				val newState = previousState.copyOf()
+				newState[actionIndex] = BEAM
+				return@mapIndexed countTimelines(
+					linesWithSplitters = linesWithSplitters.drop(1),
+					previousState = newState,
+					cache = cache,
+				)
+			}
+
+			if (action == SPLITTER) {
+				val stateAbove = previousState[actionIndex]
+				if (stateAbove == BEAM) {
+					val leftTimeline = previousState.copyOf()
+					val rightTimeline = previousState.copyOf()
+
+					leftTimeline[actionIndex - 1] = BEAM
+					leftTimeline[actionIndex] = EMPTY_SPACE
+
+					rightTimeline[actionIndex] = EMPTY_SPACE
+					rightTimeline[actionIndex + 1] = BEAM
+
+					val cacheKeyLeft = depth to actionIndex - 1
+					val countLeft = cache.getOrPut(cacheKeyLeft) {
+						countTimelines(
+							linesWithSplitters = linesWithSplitters.drop(1),
+							previousState = leftTimeline,
+							cache = cache
+						)
+					}
+
+					val cacheKeyRight = depth to actionIndex + 1
+					val countRight = cache.getOrPut(cacheKeyRight) {
+						countTimelines(
+							linesWithSplitters = linesWithSplitters.drop(1),
+							previousState = rightTimeline,
+							cache = cache
+						)
+					}
+
+					return@mapIndexed countLeft + countRight
+				}
+
+				return@mapIndexed 0L
+			}
+
+			error("action not correct")
+		}.sum()
+
+		return sum
 	}
 }
